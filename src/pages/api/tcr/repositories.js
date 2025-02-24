f30d9fb6b8f7035f64bea4dd10fbb7127053e83c
scoful <1269717999@qq.com>
2025-02-24 11:29:41 +0800

    新增cors

--- src/pages/api/tcr/repositories.js
+++ src/pages/api/tcr/repositories.js
@@ -1,29 +1,34 @@
 import {withAuth} from '@/utils/withAuth';
 import {tcrClient} from '@/utils/tcr';
+import {cors, runMiddleware} from '@/middleware/cors';
 
 const handler = async (req, res) => {
+    // 运行 CORS 中间件
+    await runMiddleware(req, res, cors);
+
     if (req.method !== 'GET') {
-        return res.status(405).json({message: '方法不允许'});
-    }
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    } else {
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.pageSize) || 10;
+        const offset = (page - 1) * pageSize;
+        const searchKey = req.query.search || "";
 
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 10;
-    const offset = (page - 1) * pageSize;
-    const searchKey = req.query.search || "";
-
-    try {
-        const result = await tcrClient.DescribeRepositoryOwnerPersonal({
-            Limit: pageSize,
-            Offset: offset,
-            RepoName: searchKey
-        });
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(500).json({
-            message: '获取仓库列表失败',
-            code: error.code,
-            error: error.message
-        });
+        try {
+            const result = await tcrClient.DescribeRepositoryOwnerPersonal({
+                Limit: pageSize,
+                Offset: offset,
+                RepoName: searchKey
+            });
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json({
+                message: '获取仓库列表失败',
+                code: error.code,
+                error: error.message
+            });
+        }
     }
 };
 
