5e10cdc1b20b077c75f1d2bbbf2375a49e0b95cb
scoful <1269717999@qq.com>
2025-03-01 12:56:11 +0800

    搜索图标增加点击功能

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,6 +142,12 @@ export default function HomePage() {
         }
     };
 
+    // 新增处理搜索的函数
+    const handleSearchClick = () => {
+        fetchRepositories(1, searchKey); // 触发搜索
+        setCurrentPage(1); // 重置到第一页
+    };
+
     const fetchNamespaces = async () => {
         try {
             const response = await fetch('/api/tcr/namespaces', {
@@ -433,19 +439,22 @@ export default function HomePage() {
                                             </svg>
                                         </button>
                                     )}
-                                    <svg
-                                        className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400 dark:text-gray-500"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                        />
-                                    </svg>
+                                    <button onClick={handleSearchClick}
+                                            className="absolute right-3 top-1/2 transform -translate-y-1/2">
+                                        <svg
+                                            className="w-4 h-4 text-gray-400 dark:text-gray-500"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth={2}
+                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                            />
+                                        </svg>
+                                    </button>
                                 </div>
                             </div>
                             <div className="flex items-center space-x-4">
