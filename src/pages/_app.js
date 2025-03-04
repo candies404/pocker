3118517857c9cdd1e1277bf0d87a523698d877ad
scoful <1269717999@qq.com>
2025-03-04 15:49:35 +0800

    增加vercel的analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import {ThemeProvider} from '@/utils/themeContext';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({Component, pageProps}) {
     return (
         <ThemeProvider>
             <Component {...pageProps} />
             <Footer/>
+            <Analytics />
         </ThemeProvider>
     );
 }
