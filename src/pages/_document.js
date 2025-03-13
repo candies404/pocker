c23f1d80931654ddbac630dc1464dfe93c87f79f
scoful <1269717999@qq.com>
2025-03-13 21:36:49 +0800

    在文档头部添加标题

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import {Head, Html, Main, NextScript} from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head/>
+            <Head>
+                <title>Pocker - Docker 镜像私服</title>
+            </Head>
             <body className="antialiased">
             <Main/>
             <NextScript/>
