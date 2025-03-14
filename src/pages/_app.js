e774f42a4188cab45dbedb88efe9ba6fda8dab21
scoful <1269717999@qq.com>
2025-03-14 11:24:32 +0800

    在_app.js中添加了meta标签以优化SEO和响应式设计，同时在_document.js中添加了字符集和主题颜色meta标签，更新了favicon链接。

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,23 @@
 import "@/styles/globals.css";
 import {ThemeProvider} from '@/utils/themeContext';
 import Footer from '@/components/Footer';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from "@vercel/speed-insights/next";
+import Head from 'next/head';
+import {APP_CONFIG} from '@/config/version';
 
 export default function App({Component, pageProps}) {
     return (
         <ThemeProvider>
-            <Component {...pageProps} />
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name="description" content="让每个人都有自己的 Docker 私服"/>
+                <title>{`${APP_CONFIG.name} - Docker 镜像私服`}</title>
+            </Head>
+            <Component {...pageProps}/>
             <Footer/>
-            <Analytics />
-            <SpeedInsights />
+            <Analytics/>
+            <SpeedInsights/>
         </ThemeProvider>
     );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,12 @@
-import {Head, Html, Main, NextScript} from "next/document";
+import {Head, Html, Main, NextScript} from 'next/document';
 
 export default function Document() {
     return (
-        <Html lang="en">
+        <Html>
             <Head>
-                <title>Pocker - Docker 镜像私服</title>
+                <meta charSet="utf-8"/>
+                <meta name="theme-color" content="#000000"/>
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
             <body className="antialiased">
             <Main/>
