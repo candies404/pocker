d48d09827f41ed045ec590b9b90f3976b3dda0cd
scoful <1269717999@qq.com>
2025-07-15 16:50:03 +0800

    更新 VERSION_CONSTANTS 中的 DISMISS_DURATION 常量，将其值从 10秒修改为 24小时（86400000毫秒），以确保版本更新通知的正确显示时长。

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // 版本更新通知相关常量
 export const VERSION_CONSTANTS = {
     NOTIFICATION_DISMISS_KEY: 'pocker_update_notification_dismissed_at',
-    DISMISS_DURATION: 10 * 1000, // 24小时的毫秒数
+    DISMISS_DURATION: 24 * 60 * 60 * 1000, // 24小时的毫秒数 (86,400,000毫秒)
 };
 
 // GitHub相关常量
